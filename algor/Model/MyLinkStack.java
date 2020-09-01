bd2c9564a8506c2f346ef5fa37a5be1fcb6a446e
asdfv1929 <asdfv1929@163.com>
2020-09-02 07:57:31 +0800

    linkstack

--- algor/Model/MyLinkStack.java
+++ algor/Model/MyLinkStack.java
@@ -10,6 +10,25 @@ public class MyLinkStack<E> implements Iterable<E>{
     private int elementCount;   // 元素数量
     public class Node {
         E val;
+        Node next;
+    }
 
+    public boolean isEmpty() { return first == null; }
+
+    public int size() { return elementCount; }
+
+    public void push(E item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.val = item;
+        first.next = oldFirst;
+        elementCount++;
+    }
+
+    public E pop() {
+        E item = first.val;
+        first = first.next;
+        elementCount--;
+        return item;
     }
 }
