aa386914701eb7460c1ae393d866e943c85c73c8
asdfv1929@163.com <asdfv1929@163.com>
2020-09-04 10:00:25 +0800

    idea

--- algor/Sort/Insertion.java
+++ algor/Sort/Insertion.java
@@ -2,6 +2,7 @@ package algor.Sort;
 
 public class Insertion extends Template {
     
+    // 交换元素
     // public static void sort(Comparable[] arr) {
     //     int len = arr.length;
     //     for (int i = 1; i < len; i++) {
@@ -11,12 +12,17 @@ public class Insertion extends Template {
     //     }
     // }
 
+    // 元素右移
     public static void sort(Comparable[] arr) {
         int len = arr.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j > 0 && less(); j--) {
-                
+        for (int i = 1; i < len; i++) {
+            Comparable temp = arr[i];
+            int j = i-1;
+            while (j >= 0 && less(temp, arr[j])) {
+                arr[j+1] = arr[j];
+                j--;
             }
+            arr[j+1] = temp;
         }
     }
 
@@ -24,8 +30,7 @@ public class Insertion extends Template {
         Integer[] arr = {5, 3, 2, 6, 8, 4};
         Insertion.sort(arr);
         Insertion.print(arr);
-        if (isSorted(arr)) 
-            System.out.println("Sorted");
+        assert isSorted(arr);
     }
 
 }
